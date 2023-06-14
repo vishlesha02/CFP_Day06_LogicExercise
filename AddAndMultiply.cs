0c441520507293d31583a34d7ebf24a606f9bde9
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-14 21:03:58 +0530

    Logics

---
+++ AddAndMultiply.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyLogicExercise
+{
+    class AddAndMultiply
+    {
+        int num1;
+        int num2;
+        int num3;
+        int result;
+
+        public void Add()
+        {
+            Console.WriteLine("Enter three numbers");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            num3 = int.Parse(Console.ReadLine());
+
+            result = (num1 + num2) * num3;
+            Console.WriteLine(result);
+                   
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyLogicExercise
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            AddAndMultiply p1 = new AddAndMultiply();
+            p1.Add();
+
+        }
+    }
+
+
+
+
+}
