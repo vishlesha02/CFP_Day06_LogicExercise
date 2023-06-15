cc2f8155f88727ce4727add0a6aa272c28b3a591
vishlesha02 <vishleshamortale18@gmail.com>
2023-06-15 17:02:22 +0530

    Sum of Fraction Added

---
+++ FractionSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyLogicExercise
+{
+    internal class FractionSum
+    {
+        int num;
+        double sum=0.0;
+        public void SumOfFraction()
+        {
+            Console.WriteLine("Enter a Number");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                sum += (1 / (double)(i * i));
+            }
+            Console.WriteLine($"Sum of Series {sum}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,50 +13,51 @@ namespace EasyLogicExercise
         public static void Main(String[] args)
         {
             /*AddAndMultiply p1 = new AddAndMultiply();
-            p1.Add();*/
+            p1.Add();
 
-            /*CtoF p2= new CtoF();
-            p2.Convert();*/
+           CtoF p2 = new CtoF();
+            p2.Convert();
 
-            /*Elementary p3=new Elementary();
-              p3.Operations();  */
+            Elementary p3 = new Elementary();
+            p3.Operations();
 
-            /*IsResultSame p4= new IsResultSame();
-            p4.Sameres();*/
+            IsResultSame p4 = new IsResultSame();
+            p4.Sameres();
 
-            /*ModuloOperations p5 = new ModuloOperations();
-            p5.Modulos();*/
+            ModuloOperations p5 = new ModuloOperations();
+            p5.Modulos();
 
-            /*Cube p6 = new Cube();
-              p6.GiveCube();*/
+            Cube p6 = new Cube();
+            p6.GiveCube();
 
-            /* AbsoluteValue p7= new AbsoluteValue();
-             p7.Value();*/
+            AbsoluteValue p7 = new AbsoluteValue();
+            p7.Value();
 
-            /*  Divisibleby2or3 p8= new Divisibleby2or3();
-              p8.Divisible();*/
+            Divisibleby2or3 p8 = new Divisibleby2or3();
+            p8.Divisible();
 
-            /* UpperCaseLetters p9 = new UpperCaseLetters();
-               p9.Letters();*/
+            UpperCaseLetters p9 = new UpperCaseLetters();
+            p9.Letters();
 
-            /* GreaterThanThirdOne p10= new GreaterThanThirdOne();
-             p10.Checks();*/
+            GreaterThanThirdOne p10 = new GreaterThanThirdOne();
+            p10.Checks();
 
 
-            /* EvenNumber p11=new EvenNumber();
-                p11.Number();*/
+            EvenNumber p11 = new EvenNumber();
+            p11.Number();
 
-            /* SortAscending p12=new SortAscending();
-             p12.SortArray(); */
+            SortAscending p12 = new SortAscending();
+            p12.SortArray();
 
-            
-            /* NeighbourAlphabet p13 = new NeighbourAlphabet();
-            p13.Neighbour();*/
+
+            NeighbourAlphabet p13 = new NeighbourAlphabet();
+            p13.Neighbour();
 
             PositiveNegZero p14 = new PositiveNegZero();
-            p14.PnZ();
+            p14.PnZ();*/
 
-
+            FractionSum p15 = new FractionSum();
+            p15.SumOfFraction();
 
 
         }
